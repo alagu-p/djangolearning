b53d75df7f76ce4b2d897a743bd94a7a0e48d890
azhagu379 <a.palanisamy@memob.com>
2022-12-14 14:25:00 +0530

    Day-88

--- auth_login_testcase/authentication/views.py
+++ auth_login_testcase/authentication/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib import messages
+from validate_email import validate_email
+
 
 class RegistrationView(View):
-    def get(self,request):
-        return render(request,'auth/register.html')
-# Create your views here.
+    def get(self, request):
+        return render(request, 'auth/register.html')
+
+    def post(self, request):
+        data = request.POST
+        print(data)
+        email = request.POST.get('email')
+        return render(request, 'auth/register.html', context={'data': data})

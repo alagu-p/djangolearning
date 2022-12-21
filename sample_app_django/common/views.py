a8aca0c0f4f1e35b955751da0efaf4bd6897f3a3
alagu-p <alagappan.plr@gmail.com>
2022-12-21 09:42:33 +0530

    Day-92

--- sample_app_django/common/views.py
+++ sample_app_django/common/views.py
@@ -4,14 +4,15 @@ from django.shortcuts import render
 from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from core.models import User
 from common.serializer import UserSerializer
+from .authentication import JWTAuthentication
 
 
 class RegisterAPIView(APIView):
 
     def post(self, request):
-        data = req uest.data
+        data = request.data
         if data['password'] != data['password_confirm']:
             raise exceptions.APIException('Password do not match !')
 
@@ -21,3 +22,27 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exceptions=True)
         serializer.save()
         return Response(serializer.data)
+class LoginAPIView(APIView):
+    def post(self,request):
+        email = request.data['email']
+        password = request.data['password']
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed('user not found')
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Incorrect password')
+        jwtAuthentications=JWTAuthentication()
+        token = jwtAuthentications.generate_jwt(user.id)
+        response = Response()
+        response.set_cookie(key='jwt',value=token,httponly=True)
+
+        response.data={
+            'messages': 'success'
+        }
+        return response
+        #     Response({
+        #     'jwt': token
+        # })
+
+        # return Response(UserSerializer(user).data)
+
